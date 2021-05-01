770752f9425a13c0314894861286d9be17e660ed
nily-fri <81628912+nily-fri@users.noreply.github.com>
2021-05-01 20:47:17 +0300

    added wind speed

--- index.js
+++ index.js
@@ -85,6 +85,9 @@ function showTemp(response) {
     response.data.main.temp
   )}°`;
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = `${Math.round(
+    response.data.wind.speed
+  )}`;
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
   document.querySelector("#feels_like").innerHTML = `${Math.round(
